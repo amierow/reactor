8b6c4ae94f290905cbd35317fbd342d21228f49f
Raghu Betina <raghu@firstdraft.com>
2017-11-24 19:20:44 +0000

    Whitelist columns in Contributor for ActiveAdmin

--- app/admin/contributor.rb
+++ app/admin/contributor.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Contributor do
 
+ permit_params :presentation_id, :name, :email, :position, :department, :facility
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
