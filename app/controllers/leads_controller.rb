e560c9032773cdb68681d6899dcb444711ca437e
Raghu Betina <raghu@firstdraft.com>
2017-11-24 19:18:41 +0000

    Generate leads resource with devise

---
+++ app/controllers/leads_controller.rb
@@ -0,0 +1,9 @@
+class LeadsController < ApplicationController
+  def index
+    @leads = Lead.all
+  end
+
+  def show
+    @lead = Lead.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :leads
+  # Routes for the Lead resource:
+  # READ
+  get "/leads", :controller => "leads", :action => "index"
+  get "/leads/:id", :controller => "leads", :action => "show"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
