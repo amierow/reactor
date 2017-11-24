574758aaec98b74bc7607bc8168dd3b9f8d65819
Raghu Betina <raghu@firstdraft.com>
2017-11-24 19:20:40 +0000

    Whitelist columns in Presentation for ActiveAdmin

--- app/admin/presentation.rb
+++ app/admin/presentation.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Presentation do
 
+ permit_params :lead_id, :date, :name
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
