734c881fa5f52558a6648bba90f7d9aff767ec2d
Raghu Betina <raghu@firstdraft.com>
2017-11-24 19:20:48 +0000

    Whitelist columns in Insight for ActiveAdmin

--- app/admin/insight.rb
+++ app/admin/insight.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Insight do
 
+ permit_params :presentation_id, :contributor_id, :insght_type_id, :title, :body
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
