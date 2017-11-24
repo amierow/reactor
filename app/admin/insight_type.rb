f051194b537b0d61ad4b9355041134f487f0125b
Raghu Betina <raghu@firstdraft.com>
2017-11-24 19:21:05 +0000

    Whitelist columns in InsightType for ActiveAdmin

--- app/admin/insight_type.rb
+++ app/admin/insight_type.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register InsightType do
 
+ permit_params :insight_type
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
