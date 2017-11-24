8054aa4a4eec6bd3c40ecc636f327a9c48e2f025
Raghu Betina <raghu@firstdraft.com>
2017-11-24 19:20:32 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "presentations#index"
   # Routes for the Insight_type resource:
   # CREATE
